4a3af97eef2111353073a4bbba833ad12ed05e3d
Jeevan522000 <61473077+Jeevan522000@users.noreply.github.com>
2023-01-06 13:07:04 +0530

    Add files via upload

---
+++ spyNumber.java
@@ -0,0 +1,40 @@
+package com.java.practice;
+
+import java.util.Scanner;
+
+public class spyNumber 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+	      System.out.println("Enter a Number");
+	      int num=sc.nextInt();
+	      int temp=num;
+	      int srev=0;
+	      while(num>0)
+	      {
+	    	  int rem=num%10;
+	    	  srev=srev+rem;
+	    	  num/=10;
+	      }
+	       num=temp;
+	      int mrev=1;
+	      while(num>0)
+	      {
+	    	  int rem=num%10;
+	    	  mrev=mrev*rem;
+	    	  num/=10;
+	      }
+	      if(srev==mrev)
+	      {
+	    	  System.out.println(temp +" is Spy Number");
+	      }
+	      else
+	      {
+	    	  System.out.println(temp +" is not Spy Number");
+	      }
+	}
+
+}
